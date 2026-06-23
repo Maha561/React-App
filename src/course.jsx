51117b1ead98a2f64a3041a9d598552959a03253
Maha561 <mahalakshmianandan04@gmail.com>
2026-06-23 15:04:34 +0530

    added

--- src/course.jsx
+++ src/course.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
 
 
 const course1="HTML" 
 
 function Course(props){
+
+    // let purchased = false;
+    
+    const[purchased,setPurchased] = useState(false)
+
+    function byCourse(){
+
+        setPurchased(true)
+
+       
+
+        
+
+    }
 
     
 
@@ -12,6 +27,8 @@ function Course(props){
             <img src={props.image} alt="" />
             <h3>{props.name}</h3>
             <p>{props.price}</p>
+            <button onClick={byCourse}>Buy now </button>
+            <p>{purchased?"already purchased" : "get it now "}</p>
         </div>
     );
 }
