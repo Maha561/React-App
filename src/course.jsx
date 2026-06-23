3838793e4c30fd3c46fcb450246da45b7cb8a978
Maha561 <mahalakshmianandan04@gmail.com>
2026-06-23 12:06:24 +0530

    added

--- src/course.jsx
+++ src/course.jsx
@@ -3,14 +3,11 @@ const course1="HTML"
 
 function Course(){
 
-    const  styles = {
-
-        backgroundColor :"green"
-    }
+    
 
     return(
         
-        <div style ={styles} >
+        <div className="card">
             <img src="" alt="" />
             <h3>{course1}</h3>
             <p>This is Maha's HTML course</p>
